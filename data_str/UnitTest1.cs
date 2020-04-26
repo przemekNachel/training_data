8c0251cf411d7d48647bf099e6c4874c50d7e93d
przemeknachel <przemeknachel@gmail.com>
2020-04-26 18:28:47 +0200

    init

---
+++ data_str/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace data_str
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
